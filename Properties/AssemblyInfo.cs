e902a45641b0db7df40a2b503cac9fa14170959c
Ruslan <maestroprog@gmail.com>
2019-05-03 18:51:16 +0300

    Синхронизация версии сборки с версией программы

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("9b0990b0-a1dd-44ae-ab0c-0510e8769860")]
 [assembly: AssemblyFileVersion("2.6.2.24")]
-[assembly: AssemblyVersion("2.6.5944.27906")]
+[assembly: AssemblyVersion("2.6.2.24")]
